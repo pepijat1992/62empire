e40a32ac7b3389d560755e615502d7be94a3b13c
phyzerbert <xian1017@outlook.com>
2019-11-27 13:09:38 +0800

    adsfsa

--- public/web/js/calculator.js
+++ public/web/js/calculator.js
@@ -1,18 +1,14 @@
 $(document).ready(function(){
-    $(".input-result").focus();
     $(".keypad-key").click(function(){
         var res = $(".input-result").val();
         if(res.length > 3) return false;
         $(".input-result").val(res + $(this).text());
-        $(".input-result").focus();
     });
     $(".keypad-clear").click(function(){
         $(".input-result").val('');
-        $(".input-result").focus();
     });
     $(".keypad-back").click(function(){
         var res = $(".input-result").val();
         $(".input-result").val(res.slice(0, -1));
-        $(".input-result").focus();
     });
 });
