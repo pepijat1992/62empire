215e3ad7a4b3207b873abca2494236a2ce84e08b
phyzerbert <xian1017@outlook.com>
2019-12-02 18:05:25 +0800

    adsfas

--- resources/views/agent/wap/index.blade.php
+++ resources/views/agent/wap/index.blade.php
@@ -21,7 +21,7 @@
                     <button class="btn btn-sm btn-primary btn-block" id="btn-add-user">{{__('words.add_player')}}</button>
                 </div>
                 <div class="col-6">
-                    <button class="btn btn-sm btn-info btn-block">{{__('words.total_report')}}</button>
+                    <a href="{{route('agent.memo')}}" class="btn btn-sm btn-info btn-block">{{__('words.memo')}}</button>
                 </div>
             </div>
             <form action="" class="" method="post">
